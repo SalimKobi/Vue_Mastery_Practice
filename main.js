dd82dc58c7bbfd629e7815056d4dc6f9b46fbe06
SalimKobi <salimkobi9@gmail.com>
2021-12-17 13:42:12 +0100

    completed the list rendering section

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpeg'
+        image: './assets/vmSocks-green-onWhite.jpeg',
+        inStock: true,
+        inventory: 9,
+        details: ["80% cotton", "20% polyster", "Gender-neutral", "Ibrahim salim kobi"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            }, {
+                variantId: 2235,
+                variantColor: "blue"
+            }, {
+                variantId: 2236,
+                variantColor: "purple"
+            }
+        ]
     }
 })
