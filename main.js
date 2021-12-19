361d0de03063896a5ae7bd265c6ad792a8d6552e
SalimKobi <salimkobi9@gmail.com>
2021-12-19 12:01:15 +0100

    finished computed properties

--- main.js
+++ main.js
@@ -1,33 +1,50 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpeg',
-        inStock: false,
+        brand: "Vue Mastery",
+        product: "Socks",
+        selectedVariant: 0,
         inventory: 9,
         details: ["80% cotton", "20% polyster", "Gender-neutral", "Ibrahim salim kobi"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: './assets/vmSocks-green-onWhite.jpeg'
+                variantImage: './assets/vmSocks-green-onWhite.jpeg',
+                variantQuantity: 10
             }, {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: './assets/vmSocks-blue-onWhite.jpeg'
+                variantImage: './assets/vmSocks-blue-onWhite.jpeg',
+                variantQuantity: 0
+
             }, {
                 variantId: 2236,
-                variantColor: "purple"
+                variantColor: "purple",
+                variantQuantity: 5
             }
         ],
         cart: 0
     },
     methods: {
-        addToCart(){
-            this.cart +=1
+        addToCart() {
+            this.cart += 1
         },
-        updateProduct: function(variantImage){
-            this.image = variantImage
+        updateProduct: function (index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
+
 })
